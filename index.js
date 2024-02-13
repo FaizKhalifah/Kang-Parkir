bd02ae5e54075006fd83c6f5e9cd374b6e42c4ec
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-02-13 21:54:58 +0700

    set up awal

---
+++ index.js
@@ -0,0 +1,8 @@
+import process from "process";
+import readlinePromises from "readline/promises";
+
+
+const input = readlinePromises.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
